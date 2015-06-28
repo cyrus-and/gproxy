dd4dabd210a079674f8387e0ce070b4b0c538036
Andrea Cardaci <cyrus.and@gmail.com>
2015-06-29 00:13:47 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+gproxy
+======
+
+gproxy exploits a Google service hosted at
+`images-onepick-opensocial.googleusercontent.com` (which is commonly used to
+fetch user-provided content, e.g., to load images by URL in Google Documents) to
+proxy arbitrary HTTP(S) traffic.
+
+Usage
+-----
+
+1. Generate a self-signed certificate (or use the one provided):
+
+        openssl req -keyout key.pem -out cert.pem \
+                -x509 -newkey rsa:2048 -nodes -subj "/CN=127.0.0.1" -days 3650
+
+2. Start the proxy:
+
+        node gproxy.js
+
+3. Use `http://127.0.0.1:8080` as a proxy server. To load HTTPS websites the
+   client must also ignore certificate errors, for example:
+
+        google-chrome --ignore-certificate-errors \
+                      --proxy-server=http://127.0.0.1:8080
+
+   or:
+
+        curl -k -x http://127.0.0.1:8080 https://example.com
+
+Caveats
+-------
+
+* Custom client headers are not forwarded to the server (e.g., no cookies).
+
+* HTTP redirect works but the client will not be aware of that so the URL will
+  not be updated.
---
+++ gproxy.js
@@ -0,0 +1,56 @@
+var fs = require('fs');
+var net = require('net');
+var http = require('http');
+var https = require('https');
+
+var HTTP_PORT = 8080;
+var HTTPS_PORT = 8443;
+
+function connect_handler(request, socket, head) {
+    var client = net.connect(HTTPS_PORT, function () {
+        socket.write('HTTP/1.1 200 Connection established\r\n\r\n', function () {
+            client.pipe(socket);
+            socket.pipe(client);
+        });
+    });
+}
+
+function request_handler(request, response) {
+    var url;
+    if (request.url[0] === '/') {
+        url = 'https://' + request.headers.host + request.url;
+    } else {
+        url = request.url;
+    }
+
+    console.log(url);
+
+    var options = {
+        'host': 'images-onepick-opensocial.googleusercontent.com',
+        'method': request.method,
+        'path': '/gadgets/proxy?container=onepick&url=' + encodeURIComponent(url)
+    };
+
+    var proxy_request = https.request(options, function (proxy_response) {
+        if (proxy_response.headers['content-disposition'] === 'attachment;filename=p.txt') {
+            delete proxy_response.headers['content-disposition'];
+        }
+        response.writeHead(proxy_response.statusCode, proxy_response.headers);
+        proxy_response.pipe(response);
+    });
+
+    request.pipe(proxy_request);
+}
+
+var http_server = http.createServer();
+var https_server = https.createServer({
+    'key': fs.readFileSync('key.pem'),
+    'cert': fs.readFileSync('cert.pem')
+});
+
+http_server.on('connect', connect_handler);
+http_server.on('request', request_handler);
+https_server.on('request', request_handler);
+
+http_server.listen(HTTP_PORT, '127.0.0.1');
+https_server.listen(HTTPS_PORT, '127.0.0.1');
